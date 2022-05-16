8de6a4e0d967780ae545be160b1748b79c610355
Miicca <105639463+Miicca@users.noreply.github.com>
2022-05-16 15:11:38 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react';
+import Carlist from './Carlist.js'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+function App() {
+  return (
+    <div>
+       <AppBar position='static'>
+        <Toolbar>
+          <Typography variant="6">
+              Carshop
+          </Typography>
+        </Toolbar>
+      </AppBar>
+     
+      <Carlist />
+     
+    </div>
+  );
+}
+
+export default App;
